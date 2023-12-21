219ca4ff4c3d63483619d3345b942bf3c08454ea
Lukas Podmelle <podmellelukas@gmail.com>
2023-12-21 22:13:51 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -32,21 +32,26 @@ const handleCompleteTodo = (id: number) => {
 
 <template>
 	<div
-		class="max-w-[900px] md:mx-auto my-20 px-6 py-4 mx-6 rounded-md shadow-md bg-white">
+		class="max-w-[900px] md:mx-auto my-20 px-6 py-4 mx-6 rounded-md shadow-md bg-white"
+	>
 		<div
-			class="flex justify-between items-center border-b border-slate-200 py-2">
-			<h1>Todo List</h1>
+			class="flex justify-between items-center border-b border-slate-200 py-2"
+		>
+			<h1>Úkoly</h1>
 			<div class="flex gap-3">
 				<div
-					class="flex flex-col sm:flex-row gap-1 items-center text-xs text-slate-500">
+					class="flex flex-col sm:flex-row gap-1 items-center text-xs text-slate-500"
+				>
 					<BanknotesIcon :class="controlsButton" />20 000 Kč
 				</div>
 				<div
-					class="flex flex-col sm:flex-row gap-1 items-center text-xs text-slate-500">
+					class="flex flex-col sm:flex-row gap-1 items-center text-xs text-slate-500"
+				>
 					<CalculatorIcon :class="controlsButton" />158:00 h
 				</div>
 				<div
-					class="flex flex-col sm:flex-row gap-1 items-center text-xs text-slate-500">
+					class="flex flex-col sm:flex-row gap-1 items-center text-xs text-slate-500"
+				>
 					<ClockIcon :class="controlsButton" />2:00 h
 				</div>
 			</div>
@@ -60,7 +65,8 @@ const handleCompleteTodo = (id: number) => {
 				:tag="task.tag"
 				:done="task.done"
 				@on-delete="handleDeleteTodo(task.id)"
-				@on-complete="handleCompleteTodo(task.id)" />
+				@on-complete="handleCompleteTodo(task.id)"
+			/>
 		</ul>
 		<div class="flex flex-col md:flex-row gap-2 mt-4">
 			<form @submit.prevent="handleAddTodo" class="contents">
@@ -68,10 +74,12 @@ const handleCompleteTodo = (id: number) => {
 					v-model="todoTitle"
 					type="text"
 					placeholder="Nový úkol"
-					class="border border-slate-200 py-2 px-3 rounded-md focus:outline-slate-200" />
+					class="border border-slate-200 py-2 px-3 rounded-md focus:outline-slate-200"
+				/>
 				<button
 					type="submit"
-					class="flex justify-center items-center gap-2 px-4 py-2 rounded-md border border-slate-200 hover:bg-slate-50">
+					class="flex justify-center items-center gap-2 px-4 py-2 rounded-md border border-slate-200 hover:bg-slate-50"
+				>
 					<PlusIcon class="w-4 h-4" /> Přidat úkol
 				</button>
 			</form>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import {
 	UserIcon,
 	CalendarIcon,
@@ -15,22 +16,31 @@ defineProps({
 	tag: String,
 	done: Boolean,
 });
-
+const active = ref(false);
 const controlsButton = 'w-4 h-4';
 </script>
 
 <template>
 	<li
-		class="flex justify-between items-center py-2 border-b border-slate-200">
+		@mouseover="active = true"
+		@mouseleave="active = false"
+		class="flex justify-between items-center py-2 border-b border-slate-200"
+		:class="{ 'bg-slate-50': active }"
+	>
 		<div class="flex items-center gap-2">
 			<input type="checkbox" @click="$emit('on-complete')" />
 			<a :class="{ 'line-through': done }">{{ title }}</a>
 			<div class="hidden md:flex gap-2 items-center">
 				<button
-					class="bg-slate-100 text-slate-400 leading-[2] text-xs px-2 rounded-tl-full rounded-tr-full rounded-br-full rounded-bl-sm">
+					class="bg-slate-200 text-slate-400 leading-[2] text-xs px-2 rounded-tl-full rounded-tr-full rounded-br-full rounded-bl-sm"
+				>
 					{{ comments }}
 				</button>
-				<div class="flex gap-2 text-slate-500">
+				<div
+					id="controls"
+					class="gap-2 text-slate-500"
+					:class="{ flex: active, hidden: !active }"
+				>
 					<button>
 						<UserIcon :class="controlsButton" />
 					</button>
@@ -49,14 +59,20 @@ const controlsButton = 'w-4 h-4';
 				</div>
 			</div>
 		</div>
-		<div class="hidden md:flex items-center gap-3">
-			<Tag v-if="tag" :text="tag" />
-			<span class="text-slate-500 text-xs">10 000 Kč</span>
-			<span class="text-slate-500 text-xs">1:30 h</span>
-			<button @click="$emit('on-delete')">
-				<TrashIcon class="w-3 h-3" />
-			</button>
+		<div class="flex items-center gap-3">
+			<div class="hidden md:flex gap-3 items-center">
+				<Tag v-if="tag" :text="tag" />
+				<span class="text-slate-500 text-xs">10 000 Kč</span>
+				<span class="text-slate-500 text-xs">1:30 h</span>
+			</div>
+			<div>
+				<button @click="$emit('on-delete')" class="hover:text-red-400">
+					<TrashIcon class="w-4 h-4 md:w-3 md:h-3" />
+				</button>
+				<button class="md:hidden">
+					<EllipsisVerticalIcon class="w-4 h-4 md:w-3 md:h-3" />
+				</button>
+			</div>
 		</div>
-		<div class="md:hidden w-4 h-4"><EllipsisVerticalIcon /></div>
 	</li>
 </template>
